77c3005fdb38e9c018e4996092973a34721a482c
HimanshuShekhar89 <shekharfun89@gmail.com>
2017-04-25 21:51:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -95,8 +95,8 @@ def makeWebhookResult(data,req):
 
     # print(json.dumps(item, indent=4))
     if req.get("result").get("action") == "yahooWeatherForecast":
-    speech = "The weather in " + location.get('city') + ": " + cond.get('text') + \
-             ", the temperature is " + cond.get('temp') + " " + units.get('temperature')
+            speech = "The weather in " + location.get('city') + ": " + cond.get('text') + \
+                     ", the temperature is " + cond.get('temp') + " " + units.get('temperature')
     print("Response:")
     print(speech)
 
